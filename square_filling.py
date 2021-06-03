cac9633a19c0417bc9c7e10f8836e339098f316f
驰 张 <2371050115@qq.com>
2021-06-03 16:15:43 +0800

    改进填充算法

--- square_filling.py
+++ square_filling.py
@@ -4,30 +4,40 @@
 # 20200715
 # 20200716
 # 20200718
+# 20210603
 
 
 import random
 from sudoku import SudokuSquare
 
 
+def _fill_cell(sdk: SudokuSquare, idx: int) -> int:
+    if idx == 81:
+        return 1
+    nums = sdk.get_possible_nums(idx)
+    if not nums:
+        return 0
+    random.shuffle(nums)
+    for num in nums:
+        sdk.num_update(idx, num)
+        if _fill_cell(sdk, idx + 1):
+            return 1
+    sdk.num_update(idx, 0)
+    return 0
+
+
 def generate_filled_square():
-    while True:
-        try:
-            source_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-            random.shuffle(source_list)
-            sdk = SudokuSquare(source_list + [0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                              0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                              0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                              0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                              0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                              0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                              0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                              0, 0, 0, 0, 0, 0, 0, 0, 0])
-            for idx in range(9, 81):
-                nums = sdk.get_possible_nums(idx)
-                num = random.choice(nums)
-                sdk.num_update(idx, num)
-        except IndexError:
-            continue
-        else:
-            return sdk
+    source_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    random.shuffle(source_list)
+    sdk = SudokuSquare(source_list + [0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                      0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                      0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                      0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                      0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                      0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                      0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                      0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+    _fill_cell(sdk, 9)
+    return sdk
+
